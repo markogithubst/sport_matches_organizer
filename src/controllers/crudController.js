eaff94817b33ac502a77dd97f89e3d0d1630fee4
vukusicmarko <121028537+vukusicmarko@users.noreply.github.com>
2023-02-13 15:00:15 +0100

    Crud controller (#3)
    * add custom error types
    
    * add crud functions to crudController
    
    * refactor functions, remove curly bracket and id from destructure params

---
+++ src/controllers/crudController.js
@@ -0,0 +1,46 @@
+const { ErrorTypes } = require('../errors/errorTypes');
+const { ServerError } = require('../errors/ServerError');
+
+const getOne = async (req, res, model) => {
+	const { id } = req.params;
+	const dataFound = await model.findById(id);
+	if(!dataFound) throw new ServerError(ErrorTypes.notFound);
+
+	res.status(200).json({ success: true, data: dataFound });
+};
+
+const getAll = async (req, res, model) => {
+	const dataFound = await model.find();
+	res.status(200).json({ success: true, data: dataFound });
+};
+
+const deleteOne = async (req, res, model) => {
+	const { id } = req.params;
+	const datadeleted = await model.findByIdAndDelete(id);
+	if(!datadeleted) throw new ServerError(ErrorTypes.notFound);
+
+	res.status(200).json({ success: true, data: datadeleted });
+};
+
+const createOne = async (req, res, model) => {
+	const dataCreated = await model.create(req.body);
+	res.status(201).json({ success: true, data: dataCreated });
+};
+
+const updateOne = async (req, res, model) => {
+	const { id } = req.params;
+	const dataUpdated = await model.findByIdAndUpdate(id, req.body, {
+		new: true,
+	});
+	if(!dataUpdated) throw new ServerError(ErrorTypes.notFound);
+    
+	res.status(200).json({ success: true, data: dataUpdated });
+};
+
+module.exports = {
+	getOne,
+	getAll,
+	deleteOne,
+	createOne,
+	updateOne
+};
---
+++ src/errors/errorTypes.js
@@ -0,0 +1,14 @@
+const ErrorTypes = {
+	notFound: {
+		status: 404,
+		message: 'Data not found'
+	},
+	notValid: {
+		status: 400,
+		message: 'Invalid request parameters'
+	}
+};
+
+module.exports = {
+	ErrorTypes
+};
