95ed1958c3eefa5561087358ebf32f96523bdfab
vukusicmarko <121028537+vukusicmarko@users.noreply.github.com>
2023-03-01 16:52:20 +0100

    Registration handle submit (#78)
    * removed unused outlet
    
    * add connect to backend

--- client/src/components/registration/Registration.jsx
+++ client/src/components/registration/Registration.jsx
@@ -1,6 +1,37 @@
+import { useState } from "react";
 import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
+import axios from "axios";
 
 const Registration = () =>{
+
+  const [formData, setFormData] = useState({
+    username: "",
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+    phone: "",
+    role: "USER"
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    axios
+      .post("http://localhost:8000/user", formData)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+  
     return(
       <div>
       <Container>
@@ -13,33 +44,33 @@ const Registration = () =>{
                 <div className="mb-5 mt-md-4">
                 <h2 className="fw-bold mb-5 text-center">Register</h2>
                   <div className="mb-3">
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                       <Form.Group className="mb-3" controlId="formBasicUsername">
                         <Form.Label className="text-center">
                           Username
                         </Form.Label>
-                        <Form.Control type="text" placeholder="Enter username" />
+                        <Form.Control type="text" placeholder="Enter username" name="username" value={formData.username} onChange={handleChange}/>
                       </Form.Group>
 
                       <Form.Group className="mb-3" controlId="formBasicFirstName">
                         <Form.Label className="text-center">
                           First Name
                         </Form.Label>
-                        <Form.Control type="text" placeholder="Enter first name" />
+                        <Form.Control type="text" placeholder="Enter first name" name="name" value={formData.name} onChange={handleChange}/>
                       </Form.Group>
 
                       <Form.Group className="mb-3" controlId="formBasicLastName">
                         <Form.Label className="text-center">
                           Last Name
                         </Form.Label>
-                        <Form.Control type="text" placeholder="Enter last name" />
+                        <Form.Control type="text" placeholder="Enter last name" name="surname" value={formData.surname} onChange={handleChange}/>
                       </Form.Group>
 
                       <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label className="text-center">
                           Email address
                         </Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email" name="email" value={formData.email} onChange={handleChange}/>
                       </Form.Group>
 
                       <Form.Group
@@ -47,7 +78,7 @@ const Registration = () =>{
                         controlId="formBasicPassword"
                       >
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" name="password" value={formData.password} onChange={handleChange}/>
                       </Form.Group>
 
                       <Form.Group
@@ -55,7 +86,7 @@ const Registration = () =>{
                         controlId="formBasicPhone"
                       >
                         <Form.Label>Phone Number</Form.Label>
-                        <Form.Control type="tel" placeholder="Enter phone number" />
+                        <Form.Control type="tel" placeholder="Enter phone number" name="phone" value={formData.phone} onChange={handleChange}/>
                       </Form.Group>
                       <div className="d-grid">
                         <Button variant="primary" type="submit">
